e9f57b0e09d66aa2e4c762eea309ca9a7c03c3ca
kaua <kauaxavier032@gmail.com>
2026-06-15 16:44:06 -0300

    feat: Add migration to create a user table with the necessary fields.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,28 +1,28 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('users')
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-    @Column({type: 'varchar', length: 255})
-    name: string;
+  @Column({ type: 'varchar', length: 255 })
+  name!: string;
 
-    @Column({ type: 'varchar', length: 150, unique: true })
-    email: string;
+  @Column({ type: 'varchar', length: 150, unique: true })
+  email!: string;
 
-    @Column({ type: 'varchar' })
-    password: string;
+  @Column({ type: 'varchar' })
+  password!: string;
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
 
-    @UpdateDateColumn({ name: 'updated_at' })
-    updatedAt: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
 }
