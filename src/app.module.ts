0641e7726776c0ad1a075807ba75e9a0c9f49703
kaua <kauaxavier032@gmail.com>
2026-06-15 18:24:26 -0300

    refactor: Reorganize the directory structure and move the user module to the 'domains' directory; remove unused DTOs and services.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './domains/users/users.module';
 
 @Module({
   imports: [
