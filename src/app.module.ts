bf90ceaba99cdf21cf4f39611f637057ac99d86c
kaua <kauaxavier032@gmail.com>
2026-06-15 18:34:26 -0300

    refactor: update TypeOrmModule initialization to use async configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,10 @@ import { UsersModule } from './domains/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      injects: [ConfigService],
-      useFactory: (ConfigService: ConfigService) => configService.get('database'),
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get('database'),
     }),
     UsersModule,
   ],
