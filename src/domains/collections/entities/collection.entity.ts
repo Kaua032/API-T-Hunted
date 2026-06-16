7eab5e2afbbed1e9735c4c715fe44225049057ee
`kaua` <`kauaxavier032@gmail.com`>
2026-06-16 09:48:38 -0300

    feat: Add Collection entity, migration, and update User entity for relationships

--- src/domains/collections/entities/collection.entity.ts
+++ src/domains/collections/entities/collection.entity.ts
@@ -1,1 +1,37 @@
-export class Collection {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, Unique } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Car } from '../../cars/entities/car.entity';
+
+export enum CarCondition {
+  LOOSE = 'loose',
+  CARDED = 'carded',
+}
+
+@Entity('collections')
+@Unique(['user', 'car', 'condition'])
+export class Collection {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'int', default: 1 })
+  quantity!: number;
+
+  @Column({
+    type: 'enum',
+    enum: CarCondition,
+    default: CarCondition.LOOSE,
+  })
+  condition!: CarCondition;
+
+  @ManyToOne(() => User, (user) => user.collections, { onDelete: 'CASCADE' })
+  user!: User;
+
+  @ManyToOne(() => Car, { onDelete: 'CASCADE' })
+  car!: Car;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
--- src/domains/users/entities/user.entity.ts
+++ src/domains/users/entities/user.entity.ts
@@ -4,21 +4,26 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany
 } from 'typeorm';
+import { Collection } from '../../collections/entities/collection.entity';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
-
+  
   @Column({ type: 'varchar', length: 255 })
   name!: string;
-
+  
   @Column({ type: 'varchar', length: 150, unique: true })
   email!: string;
-
+  
   @Column({ type: 'varchar' })
   password!: string;
+
+  @OneToMany(() => Collection, (collection) => collection.user)
+  collections!: Collection[];
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt!: Date;
