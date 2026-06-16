953a8347a0970361f339ebfcba00a62f64ae94d7
kaua <kauaxavier032@gmail.com>
2026-06-15 21:30:42 -0300

    feat: implement login method in AuthService with JWT and password validation

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(email: string, pass: string) {
+    const user = await this.usersService.findByEmail(email);
+    
+    if (!user) {
+      throw new UnauthorizedException('E-mail ou palavra-passe incorretos.');
+    }
+
+    const isPasswordValid = await bcrypt.compare(pass, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('E-mail ou palavra-passe incorretos.');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
