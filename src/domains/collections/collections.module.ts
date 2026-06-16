a5bb07b6864d592d07d3fac67b142cb015314b31
`kaua` <`kauaxavier032@gmail.com`>
2026-06-16 09:54:47 -0300

    feat: Add TypeOrmModule import for Collection entity in CollectionsModule

--- src/domains/collections/collections.module.ts
+++ src/domains/collections/collections.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectionsService } from './collections.service';
 import { CollectionsController } from './collections.controller';
+import { Collection } from './entities/collection.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Collection])],
   controllers: [CollectionsController],
   providers: [CollectionsService],
 })
