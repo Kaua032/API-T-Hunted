3f9ef326e41d5915f4a832f33caae05fe985e3b8
kaua <kauaxavier032@gmail.com>
2026-06-15 22:21:56 -0300

    feat: Add pagination properties to SearchCarDto with default values

--- src/domains/cars/dto/search-car.dto.ts
+++ src/domains/cars/dto/search-car.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, IsNumber, IsBoolean } from 'class-validator';
+import { IsOptional, IsString, IsNumber, IsBoolean, Min } from 'class-validator';
 import { Type, Transform } from 'class-transformer';
 
 export class SearchCarDto {
@@ -24,4 +24,16 @@ export class SearchCarDto {
   @Transform(({ value }) => value === 'true')
   @IsBoolean()
   isSth?: boolean;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  limit?: number = 10;
 }
