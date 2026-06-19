51e439917e6ff1bfec01d147a4e0006f7d757ca9
kaua <kauaxavier032@gmail.com>
2026-06-18 23:19:26 -0300

    feat: Enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
 
   app.use(helmet());
   app.setGlobalPrefix('api/v1');
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
